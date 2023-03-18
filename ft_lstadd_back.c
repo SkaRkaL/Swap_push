88c91d776b3f4c54a926daf9d6ea60345b440568
skarkal <karkalsaad@gmail.com>
2023-03-18 16:45:42 +0100

    list add back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,18 @@
+#include "push_swap.h"
+
+static void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		return (ft_lstadd_front(lst, new));
+	ft_lstlast(*lst)->next = new;
+}
