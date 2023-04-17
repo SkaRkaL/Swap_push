9cb4cc01ff9323f92c3618eafe773fa4efab469b
skarkal <karkalsaad@gmail.com>
2023-04-17 23:50:47 +0000

    Norm : OK

--- rvrse-rotate.c
+++ rvrse-rotate.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rvrse-rotate.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:12:51 by sakarkal          #+#    #+#             */
+/*   Updated: 2023/04/17 23:50:22 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 static void	reverse_rotate(t_list **stack)
 {
-	t_list *tmp;
+	t_list	*tmp;
 
 	tmp = ft_beforelast((*stack));
 	ft_lstadd_front(stack, tmp->next);
@@ -12,7 +24,7 @@ static void	reverse_rotate(t_list **stack)
 void	rra(t_list **stack_a, int index)
 {
 	reverse_rotate(stack_a);
-	if(index)
+	if (index)
 		write(1, "rra\n", 4);
 	indx_stack(stack_a);
 }
@@ -20,7 +32,7 @@ void	rra(t_list **stack_a, int index)
 void	rrb(t_list **stack_b, int index)
 {
 	reverse_rotate(stack_b);
-	if(index)
+	if (index)
 		write(1, "rrb\n", 4);
 	indx_stack(stack_b);
 }
