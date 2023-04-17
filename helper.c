e00c3b3ccc60871369bdda7b7e496ccda7e54981
skarkal <karkalsaad@gmail.com>
2023-04-17 23:39:50 +0000

    Norm : OK

--- helper.c
+++ helper.c
@@ -1,20 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helper.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:12:06 by sakarkal          #+#    #+#             */
+/*   Updated: 2023/04/17 23:39:30 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 void	move_while(t_list **stack_a, t_list **stack_b, t_elm *elm)
 {
-	while(elm->second_tmp)
+	while (elm->second_tmp)
 	{
-		if (elm->b_tmp->content > elm->first_tmp->content && elm->b_tmp->content < elm->second_tmp->content)
+		if (elm->b_tmp->content > elm->first_tmp->content \
+			&& elm->b_tmp->content < elm->second_tmp->content)
 		{
-			elm->b_tmp->movs = 1 + up_or_down_moves(elm->second_tmp->indx, elm->b_tmp->indx, *stack_a, *stack_b);
+			elm->b_tmp->movs = 1 + up_or_down_moves(elm->second_tmp->indx, \
+				elm->b_tmp->indx, *stack_a, *stack_b);
 			elm->b_tmp->place = elm->second_tmp;
-			break;
+			break ;
 		}
-		if (elm->b_tmp->content < elm->first_tmp->content && elm->b_tmp->content > ft_lstlast(elm->first_tmp)->content)
+		if (elm->b_tmp->content < elm->first_tmp->content \
+			&& elm->b_tmp->content > ft_lstlast(elm->first_tmp)->content)
 		{
-			elm->b_tmp->movs = 1 + up_or_down_moves(elm->first_tmp->indx, elm->b_tmp->indx, *stack_a, *stack_b);
+			elm->b_tmp->movs = 1 + up_or_down_moves(elm->first_tmp->indx, \
+				elm->b_tmp->indx, *stack_a, *stack_b);
 			elm->b_tmp->place = elm->first_tmp;
-			break;
+			break ;
 		}
 		elm->second_tmp = elm->second_tmp->next;
 		elm->first_tmp = elm->first_tmp->next;
