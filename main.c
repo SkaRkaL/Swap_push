b63316c7db2b8e035182aae052928cd042534fb9
skarkal <karkalsaad@gmail.com>
2023-04-18 03:13:46 +0000

    Norm : OK

--- main.c
+++ main.c
@@ -1,50 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:12:10 by sakarkal          #+#    #+#             */
+/*   Updated: 2023/04/18 03:06:29 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
-
-void aff(t_list *stack_a, t_list *stack_b, int size)
-{
-	printf("------Start-----\n");
-	while (size)
-	{
-		if (stack_a)
-		{
-			printf("%d| %d\t", stack_a->content, stack_a->indx);
-			stack_a = stack_a->next;
-		}
-		else
-		{
-			printf("  |   \t");
-		}
-		if (stack_b)
-		{
-			printf("%d| %d", stack_b->content, stack_b->movs);
-			stack_b = stack_b->next;
-		}
-		size--;
-		printf("\n");
-	}
-	printf("-\t-\na\tb\n");
-	printf("------End-----\n");
-}
-
-void is_sorted(t_list *stack_a)
-{
-	t_list *tmp;
-
-	tmp = stack_a->next;
-	while (stack_a->next)
-	{
-		tmp = stack_a->next;
-
-		while (tmp)
-		{
-			if (stack_a->content > tmp->content)
-				return;
-			tmp = tmp->next;
-		}
-		stack_a = stack_a->next;
-	}
-	exit(write(1, "Already Sorted :)\n", 19) & 0);
-}
 
 void	__find_lis_head(t_list **stack, int full_size)
 {
@@ -56,11 +22,13 @@ void	__find_lis_head(t_list **stack, int full_size)
 	inddexing_flag(stack, ret, full_size);
 }
 
-void indx_stack(t_list **stack)
+void	indx_stack(t_list **stack)
 {
-	t_list *tompo;
+	int		i;
+	t_list	*tompo;
+
+	i = 0;
 	tompo = *stack;
-	int i = 0;
 	while (tompo)
 	{
 		tompo->indx = i;
@@ -69,22 +37,14 @@ void indx_stack(t_list **stack)
 	}
 }
 
-void fack_swap(t_list **stack)
+t_list	*get_best_move(t_list *stack_b)
 {
-	int tmp;
+	t_list	*tmp;
+	int		i;
 
-	if (!*stack || !(*stack)->next)
-		return;
-	tmp = (*stack)->content;
-	(*stack)->content = (*stack)->next->content;
-	(*stack)->next->content = tmp;
-}
-
-t_list *get_best_move(t_list *stack_b)
-{
+	tmp = stack_b;
+	i = ft_lstsize(stack_b);
 	stack_b->min_move = INT_MAX;
-	t_list *tmp = stack_b;
-	int i = ft_lstsize(stack_b);
 	while (i--)
 	{
 		if (tmp->movs < stack_b->min_move)
@@ -101,114 +61,27 @@ t_list *get_best_move(t_list *stack_b)
 	return (NULL);
 }
 
-t_list *min_element(t_list *a)
+int	main(int ac, char **av)
 {
-	int min = a->content;
-	t_list *tmp = a;
-	while (tmp)
-	{
-		if (min > tmp->content)
-		{
-			min = tmp->content;
-		}
-		tmp = tmp->next;
-	}
-	while (a)
-	{
-		if (a->content == min)
-			return (a);
-		a = a->next;
-	}
-	return (NULL);
-}
-
-t_list *max_element(t_list *stack)
-{
-	int max = stack->content;
-	t_list *tmp = stack;
-	while (tmp->next)
-	{
-		if (max < tmp->next->content)
-		{
-			max = tmp->next->content;
-		}
-		tmp = tmp->next;
-	}
-	while (stack)
-	{
-		if (stack->content == max)
-			return (stack);
-		stack = stack->next;
-	}
-	return (NULL);
-}
-
-int main(int ac, char **av)
-{
-	int		i;
-	int		j;
 	char	**c;
+	int		i;
+	t_list	*stack_a;
+	t_list	*stack_b;
 
+	stack_a = NULL;
+	stack_b = NULL;
 	i = 0;
-	if (ac > 1)
-	{
-		if (av[1][0] == '\0')
-			return (write(2, "Error\n", 7));
-		c = ft_split(ft_strjoin(ac - 1, av + 1, " "), ' ');
-		while (c[i])
-		{
-			if (ft_isdigit(c[i]))
-				return (write(2, "Error\n", 7));
-			i++;
-		}
-		i = 0;
-		while (c[i])
-		{
-			j = i + 1;
-			while (c[j])
-			{
-				if (!strcmp(c[i], c[j]))
-					return (write(2, "Error\n", 7));
-				j++;
-			}
-			i++;
-		}
-	}
-	else
-		return (0);
-	t_list *stack_a = NULL;
-	t_list *stack_b = NULL;
-
-
-	for (i = 0; c[i]; i++)
-	{
-		ft_lstadd_back(&stack_a, ft_lstnew(atoi(c[i])));
-	}
-	int size;
-	size = i;
+	c = _parcer(ac, av);
+	if (c == NULL)
+		return (check_isfree(c), write(2, "Error\n", 7));
+	if (__main_help(&c, &stack_a, &i))
+		return (write(2, "Error\n", 7));
 	is_sorted(stack_a);
-	t_list *tempe_head = ft_lstlast(stack_a);
-	if (size <= 3)
-	{
-		sort_III(&stack_a);
-	}
-	if (size > 3)
-	{
-		if (tempe_head->next == NULL)
-			tempe_head->next = stack_a;
-		__find_lis_head(&stack_a, size);
-		if (tempe_head->next == stack_a)
-			tempe_head->next = NULL;
-		indx_stack(&stack_a);
-		push_flag0_b(&stack_a, &stack_b);
-		i = ft_lstsize(stack_b);
-		while (i--)
-		{
-			moves_indx(&stack_a, &stack_b);
-			push_b_to_a(&stack_b, &stack_a, get_best_move(stack_b));
-		}
-		mn_te7t_lfo9(&stack_a, min_element(stack_a), fun(stack_a, min_element(stack_a)));
-		aff(stack_a, stack_b, ft_lstsize(stack_a));
-	}
+	if (i <= 3)
+		sort_3(&stack_a);
+	if (i > 3)
+		sort_big(&stack_a, &stack_b, i);
+	check_isfree(c);
+	ft_lstclear(&stack_a);
 	return (0);
 }
---
+++ main_helper.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_helper.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 00:50:28 by sakarkal          #+#    #+#             */
+/*   Updated: 2023/04/18 03:09:15 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*max_element(t_list *stack)
+{
+	int		max;
+	t_list	*tmp;
+
+	max = stack->content;
+	tmp = stack;
+	while (tmp->next)
+	{
+		if (max < tmp->next->content)
+		{
+			max = tmp->next->content;
+		}
+		tmp = tmp->next;
+	}
+	while (stack)
+	{
+		if (stack->content == max)
+			return (stack);
+		stack = stack->next;
+	}
+	return (NULL);
+}
+
+t_list	*min_elmt(t_list *a)
+{
+	int		min;
+	t_list	*tmp;
+
+	tmp = a;
+	min = a->content;
+	while (tmp)
+	{
+		if (min > tmp->content)
+		{
+			min = tmp->content;
+		}
+		tmp = tmp->next;
+	}
+	while (a)
+	{
+		if (a->content == min)
+			return (a);
+		a = a->next;
+	}
+	return (NULL);
+}
+
+int	__main_help(char ***c, t_list **stack_a, int *i)
+{
+	long	i_atoi;
+
+	i_atoi = 0;
+	while ((*c)[*i])
+	{
+		i_atoi = ft_atoi((*c)[(*i)++]);
+		if (i_atoi > INT_MAX || i_atoi < INT_MIN)
+		{
+			check_isfree(*c);
+			return (1);
+		}
+		ft_lstadd_back(stack_a, ft_lstnew(i_atoi));
+	}
+	return (0);
+}
+
+void	sort_big(t_list **stack_a, t_list **stack_b, int size)
+{
+	int		i;
+	t_list	*tempe_head;
+
+	*stack_b = NULL;
+	tempe_head = ft_lstlast(*stack_a);
+	if (tempe_head->next == NULL)
+		tempe_head->next = *stack_a;
+	__find_lis_head(stack_a, size);
+	if (tempe_head->next == *stack_a)
+		tempe_head->next = NULL;
+	indx_stack(stack_a);
+	push_flag0_b(stack_a, stack_b);
+	i = ft_lstsize(*stack_b);
+	while (i--)
+	{
+		moves_indx(stack_a, stack_b);
+		push_b_to_a(stack_b, stack_a, get_best_move(*stack_b));
+	}
+	nrml_sort(stack_a, min_elmt(*stack_a), fun(*stack_a, min_elmt(*stack_a)));
+}
