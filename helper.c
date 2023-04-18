d2bdee95f4f5a4023a086905eb35fb1ae989f026
skarkal <karkalsaad@gmail.com>
2023-04-18 03:13:16 +0000

    Norm : OK

--- helper.c
+++ helper.c
@@ -6,11 +6,45 @@
 /*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 21:12:06 by sakarkal          #+#    #+#             */
-/*   Updated: 2023/04/17 23:43:38 by sakarkal         ###   ########.fr       */
+/*   Updated: 2023/04/18 03:05:47 by sakarkal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_lstclear(t_list **lst)
+{
+	t_list	*yes;
+
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		yes = (*lst)->next;
+		free(*lst);
+		*lst = yes;
+	}
+	*lst = NULL;
+}
+
+void	is_sorted(t_list *stack_a)
+{
+	t_list	*tmp;
+
+	tmp = stack_a->next;
+	while (stack_a->next)
+	{
+		tmp = stack_a->next;
+		while (tmp)
+		{
+			if (stack_a->content > tmp->content)
+				return ;
+			tmp = tmp->next;
+		}
+		stack_a = stack_a->next;
+	}
+	exit(write(1, "Already sorted\n", 16) && 0);
+}
 
 void	move_while(t_list **stack_a, t_list **stack_b, t_elm *elm)
 {
